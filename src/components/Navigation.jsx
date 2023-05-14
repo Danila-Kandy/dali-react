ca8290df98f035bb6d7882ed0381a86e947a86ce
Danja <kandybovich.danik@gmail.com>
2023-05-14 17:06:47 +0300

    4 commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import getMaxId from './GetMaxId'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 export default function Navigation({ prevId, nextId, name }) {
   return (
@@ -11,24 +13,25 @@ export default function Navigation({ prevId, nextId, name }) {
         to={`/list/${prevId === 0 ? `${getMaxId()}` : prevId}`}
         className=" order-first"
       >
-        <img
+        <LazyLoadImage
           src="/img/strel1.png"
           alt=""
           className="border-[#4056A1] border-2 shadow-md border-opacity-20 md:border-0 md:shadow-none rounded-md md:m-0 h-10 md:h-20 w-auto "
-        />
+        ></LazyLoadImage>
       </Link>
       <Link to={'/list/1'} className="absolute" id="lookman">
-        <img
+        <LazyLoadImage
           src="/img/look.png"
           alt=""
           className="h-40 w-auto opacity-0 2xl:opacity-100"
-        />
+          effect="blur"
+        ></LazyLoadImage>
       </Link>
       <Link
         to={`/list/${nextId === getMaxId() + 1 ? '1' : nextId}`}
         className="order-last"
       >
-        <img
+        <LazyLoadImage
           src="/img/strel2.png"
           alt=""
           className="border-[#4056A1] border-2 shadow-md border-opacity-20 md:border-0 md:shadow-none rounded-md md:m-0 h-10 md:h-20 w-auto"
--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 export default function Error() {
   return (
     <div className="flex flex-col justify-center items-center my-56">
-      <h1 className="text-5xl text-[#E85A4F]">404</h1>
-      <h1 className="text-5xl text-[#8E8D8A]">Page Not Found</h1>
-      <p className="text-3xl text-[#8E8D8A] ">
+      <h1 className="text-5xl ">404</h1>
+      <h1 className="text-5xl ">Page Not Found</h1>
+      <p className="text-3xl ">
         Go
-        <Link className="underline hover:text-[#E85A4F] ml-1" to="/">
+        <Link className="underline hover:text-[#4056A1] ml-1" to="/">
           Home
         </Link>
       </p>
